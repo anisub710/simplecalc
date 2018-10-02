8d5e4c26d08495dac57a0db2fd9ef0275c62a871
Anirudh Kumar Subramanyam <ask710@uw.edu>
2018-10-02 00:30:42 -0700

    Add completed code before test and ec

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,55 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        var result:Int = 0
+        if args.count == 3 {
+            guard let val1 = Int(args[0]) else{
+                return -1
+            }
+            guard let val2 = Int(args[2]) else {
+                return -1
+            }
+            switch args[1]{
+            case "+":
+                result = val1 + val2
+            case "-":
+                result = val1 - val2
+            case "/":
+                result = val1 / val2
+            case "%":
+                result = val1 - (val1 / val2) * val2
+            case "*":
+                result = val1 * val2
+            default:
+                break
+            }
+        } else {
+            let operation = args[args.count - 1]
+            switch operation{
+            case "count":
+                result = args.count - 1
+            case "avg":
+                if args.count > 1 {
+                    var sum = 0
+                    for i in 0...args.count - 2 {
+                        sum = sum + Int(args[i])!
+                    }
+                    result = sum / (args.count - 1)
+                }
+            case "fact":
+                guard let num = Int(args[0]) else{
+                    return 0
+                }
+                if num == 0 {return 1}
+                result = 1
+                for i in 1...num {
+                    result = i * result
+                }
+            default:
+                break
+            }
+        }
+        return result
     }
     
     public func calculate(_ arg: String) -> Int {
